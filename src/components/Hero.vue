2cb254eb0c4f2caa297ff4a9bc93337595fe4a5f
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2025-06-20 15:37:14 +0200

    fix: layout

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -7,7 +7,7 @@ defineProps<{
 </script>
 
 <template>
-    <section class="grid self-center justify-self-center py-12 p-6 sm:pt-24 sm:pb-16 max-w-[620px] text-center gap-3">
+    <section class="grid py-12 p-6 sm:pt-24 sm:pb-16 max-w-[620px] text-center gap-3">
         <h1 class="text-neutral-grey-400 text-4xl font-light">{{ title }}</h1>
         <h2 class="text-neutral-grey-500 text-4xl font-bold pb-2">{{ subtitle }}</h2>
         <p class="text-neutral-grey-500">{{ description }}</p>
